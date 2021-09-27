bfbbcf401df668c4366838e363de31395b58fc2d
sserrano07 <sergio.serrano@bkool.com>
2021-09-27 14:42:31 +0200

    Updated

--- vpc.tf
+++ vpc.tf
@@ -2,12 +2,12 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "${var.project_name}-${var.environment}"
-  cidr = "10.0.0.0/16"
+  cidr = "10.104.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  intra_subnets   = ["10.0.51.0/24", "10.0.52.0/24", "10.0.53.0/24"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  private_subnets = ["10.104.0.0/18", "10.104.64.0/18", "10.104.128.0/18"]
+  public_subnets  = ["10.104.200.0/24", "10.104.202.0/24", "10.104.204.0/24"]
+  intra_subnets   = ["10.104.51.0/24", "10.104.52.0/24", "10.104.53.0/24"]
 
   enable_nat_gateway = true
 
